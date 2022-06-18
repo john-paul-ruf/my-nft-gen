559ded1818f8199b2bc8e5d771f5fe0c46cce401
john.paul.ruf <john.paul.ruf@gmail.com>
2022-06-18 16:14:04 -0500

    THE ONE TWENTIES RUN RESTART

--- src/logic/animate.js
+++ src/logic/animate.js
@@ -27,28 +27,26 @@ export const animate = async (config) => {
 
         const frames = [];
 
-        const applyEffect = async(img, effects) => {
-            for(let i = 0; i < effects.length; i++){
+        const applyEffect = async (img, effects) => {
+            for (let i = 0; i < effects.length; i++) {
                 await effects[i].invokeEffect(img, frame, config.numberOfFrame);
             }
         }
 
-        const getExtraLayers = (effects,w, h) => {
+        const getExtraLayers = (effects, w, h) => {
             const extraLayers = [];
-            for(let i = 0; i < effects.length; i++){
-                extraLayers.push(new Jimp(w,h))
+            for (let i = 0; i < effects.length; i++) {
+                extraLayers.push(new Jimp(w, h))
             }
             return extraLayers;
         }
 
         const processExtraLayers = async () => {
-            for(let i = 0; i < extraLayers.length; i++){
+            for (let i = 0; i < extraLayers.length; i++) {
                 await applyEffect(extraLayers[i], [extraEffects[i]]);
 
-                if(extraEffects[i].additionalEffects.length > 0){
-                    for(let a = 0; a < extraLayers.length; a++) {
-                        await applyEffect(extraLayers[i], extraEffects[i].additionalEffects);
-                    }
+                if (extraEffects[i].additionalEffects.length > 0) {
+                    await applyEffect(extraLayers[i], extraEffects[i].additionalEffects);
                 }
             }
         }
@@ -71,18 +69,20 @@ export const animate = async (config) => {
         ////////////////////////
         //COMPOSE
         ////////////////////////
-        extraLayers.sort((x,y) => { return (x.baseLayer === y.baseLayer)? 0 : x? -1 : 1;} )
-        for(let i = 0; i < extraLayers.length; i++){
+        extraLayers.sort((x, y) => {
+            return (x.baseLayer === y.baseLayer) ? 0 : x ? -1 : 1;
+        })
+        for (let i = 0; i < extraLayers.length; i++) {
             await background.composite(extraLayers[i], 0, 0, {
                 mode: Jimp.BLEND_SOURCE_OVER,
             })
         }
 
-        await background.composite(summons, 0,0, {
+        await background.composite(summons, 0, 0, {
             mode: Jimp.BLEND_SOURCE_OVER,
         })
 
-        await background.composite(focus,0,0, {
+        await background.composite(focus, 0, 0, {
             mode: Jimp.BLEND_SOURCE_OVER,
         });
 
