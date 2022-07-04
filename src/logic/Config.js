e007642bac13e81a2e5b20912aee0d13484a1791
john.paul.ruf <john.paul.ruf@gmail.com>
2022-07-04 07:23:08 -0500

    var x (halve color depth) - goal: under 100 mb

--- src/logic/Config.js
+++ src/logic/Config.js
@@ -38,7 +38,7 @@ export class Config {
         this.sigName = sigList[getRandomInt(0, sigList.length)];
 
         this.finalImageSize = 2000;
-        this.colorDepth = 256;
+        this.colorDepth = 128;
         this.frameInc = 1;
         this.numberOfFrame = 72;
 
