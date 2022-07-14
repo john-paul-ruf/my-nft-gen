63871f67523191d60c714d7e96dfe11622468a8a
John Paul Ruf <john.paul.ruf@gmail.com>
2022-07-13 20:07:26 -0500

    better comments..?

--- src/logic/findValue.js
+++ src/logic/findValue.js
@@ -9,18 +9,18 @@
 **/
 export const findValue = (min, max, times, totalFrame, currentFrame) => {
     const range = max - min; //the range
-    const segment = totalFrame / times;  //how many times to navigate the range
+    const segment = totalFrame / times;  //Segment is the number of frames if we only did the effect once
     const halfSegment = segment / 2;  //number of frame to go up and back with in a given time
     //the magic: frame segment is the current frame number if we only did this one time. Modulus operator
     const frameSegment = currentFrame % segment;
     const step = range / halfSegment; //How much to increment in a single frame
 
     if (frameSegment <= halfSegment) { //if we haven't reached the midway point
-        //bottom of range plus how much to increment per frame time the current frame for the segment
+        //bottom of range plus how much to increment per frame times the current frame for the segment
         return min + (step * frameSegment);
     }
 
     //we are past the halfway point
-    //max of the range minus how much to increment per frame time the current frame in reverse
+    //max of the range minus how much to increment per frame times the current frame in reverse
     return max - (step * (frameSegment - halfSegment));
 }
