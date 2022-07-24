12565a551c53151bc1910a9095b181547b0f2906
john.paul.ruf <john.paul.ruf@gmail.com>
2022-07-24 17:45:58 -0500

    the-colors-test-run

--- src/logic/random.js
+++ src/logic/random.js
@@ -1,8 +1,6 @@
 //I stole this code of the net
 export const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 //Same
