81c8a5a8bc0f610f10edcddb64c1017a0af8f58d
john.paul.ruf <john.paul.ruf@gmail.com>
2022-07-30 15:02:06 -0500

    refactor: effects folder, new secondary effects

--- src/effects/secondaryEffects/sepia.js
+++ src/effects/secondaryEffects/sepia.js
@@ -1,29 +1,18 @@
-import {findValue} from "../../logic/findValue.js";
-import {getRandomIntInclusive} from "../../logic/random.js";
-
-const config = {
-    lowerRange: {lower: 1, upper: 10},
-    upperRange: {lower: 10, upper: 20},
-    times: {lower: 1, upper: 4},
-}
+const config = {}
 
 const generate = () => {
 
     const data =
         {
-            lower: getRandomIntInclusive(config.lowerRange.lower, config.lowerRange.upper),
-            upper: getRandomIntInclusive(config.upperRange.lower, config.upperRange.upper),
-            times: getRandomIntInclusive(config.times.lower, config.times.upper),
             getInfo: () => {
-                return `${sepiaEffect.name}: ${data.times} times, ${data.lower.toFixed(3)} to ${data.upper.toFixed(3)}`
+                return `${sepiaEffect.name}`
             }
         }
     return data;
 }
 
 const sepia = async (data, img, currentFrame, totalFrames, card) => {
-    const sepiaGaston = findValue(data.lower, data.upper, data.times, totalFrames, currentFrame)
-    await img.sepia(sepiaGaston);
+    await img.sepia();
 }
 
 export const effect = {
