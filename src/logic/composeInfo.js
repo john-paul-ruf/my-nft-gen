0376e39be8bad03e8caff20b225bf1ed17287406
john.paul.ruf <john.paul.ruf@gmail.com>
2022-07-30 05:56:09 -0500

    the-mp4-run

--- src/logic/composeInfo.js
+++ src/logic/composeInfo.js
@@ -8,23 +8,26 @@ import {HUE, IMAGESIZE, SCHEME, VARIATION} from "./gobals.js";
  */
 export const composeInfo = (config, effects) => {
 
-    const getEffectInfo = (effect) => {
+    //This function makes use of the newline character \n and spaces for formatting
+    //https://stackoverflow.com/questions/1155678/what-is-the-javascript-string-newline-character
+    const getEffectInfo = (effectList) => {
         let results = '';
-        for (let i = 0; i < effect.length; i++) {
-            let data = `\n  ` + effect[i].getInfo()
+        for (let i = 0; i < effectList.length; i++) { //for each effect in the effectList
+            let data = `\n  ` + effectList[i].getInfo() //start new string with effect info
 
-            if (effect[i].additionalEffects.length > 0) {
-                data = data + `\n    with additional effects: `
-                for (let a = 0; a < effect[i].additionalEffects.length; a++) {
-                    data = data + `\n      ` + effect[i].additionalEffects[a].getInfo();
+            if (effectList[i].additionalEffects.length > 0) {  // if this effect has additional effects
+                data = data + `\n    with additional effects: ` //append additional effects to the string we started above
+                for (let a = 0; a < effectList[i].additionalEffects.length; a++) {
+                    data = data + `\n      ` + effectList[i].additionalEffects[a].getInfo(); //Append additional info
                 }
             }
 
-            results = results + data;
+            results = results + data; //Append this effects total info to the final result
         }
-        return results;
+        return results; //returns the result, which is appended to the info string.
     }
 
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
     const info =
         `Title: ${config.finalFileName}\nRun: ${config.runName}\nHue: ${HUE}\nScheme: ${SCHEME}\nVariation: ${VARIATION}\nArtist: ${config._INVOKER_}\nImage Size: ${IMAGESIZE}x${IMAGESIZE} pixels\nNumber of Frames: ${config.numberOfFrame}\n\nEffects: ${getEffectInfo(effects)}`
 
