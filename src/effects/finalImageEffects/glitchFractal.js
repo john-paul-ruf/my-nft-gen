c422a1db5a4911c75178853ea5eda40d0cf7183c
john.paul.ruf <john.paul.ruf@gmail.com>
2022-07-31 20:40:47 -0500

    glitch work

--- src/effects/finalImageEffects/glitchFractal.js
+++ src/effects/finalImageEffects/glitchFractal.js
@@ -1,35 +1,29 @@
 import {getRandomIntInclusive} from "../../logic/random.js";
-import {findValue} from "../../logic/findValue.js";
 
 const config = {
-    lowerRange: {lower: 1, upper: 5},
-    upperRange: {lower: 5, upper: 10},
+    theRandom: {lower: 2, upper: 10},
     times: {lower: 1, upper: 4},
 }
 
 const generate = () => {
 
     const data = {
-        lower: getRandomIntInclusive(config.lowerRange.lower, config.lowerRange.upper),
-        upper: getRandomIntInclusive(config.upperRange.lower, config.upperRange.upper),
+        theRandom: getRandomIntInclusive(config.theRandom.lower, config.theRandom.upper),
         times: getRandomIntInclusive(config.times.lower, config.times.upper),
         getInfo: () => {
-            return `${glitchFractalEffect.name} ${data.times} times, ${data.lower} to ${data.upper}`
+            return `${glitchFractalEffect.name} random: ${data.theRandom}`
         }
     }
     return data;
 }
 
-const glitchFractal = async (data, img, currentFrame, totalFrames) => {
-
-    let theRandomGaston = Math.floor(findValue(data.lower, data.upper, data.times, currentFrame, totalFrames));
-
+const glitchFractal = async (data, img) => {
     /////////////////////
     // https://github.com/JKirchartz/Glitchy3bitdither/blob/master/source/glitches/fractal.js
     /////////////////////
     for (let j = 0; j < img.bitmap.data.length; j++) {
-        if (parseInt(img.bitmap.data[(j * theRandomGaston) % img.bitmap.data.length], 10) < parseInt(img.bitmap.data[j], 10)) {
-            img.bitmap.data[j] = img.bitmap.data[(j * theRandomGaston) % img.bitmap.data.length];
+        if (parseInt(img.bitmap.data[(j * data.theRandom) % img.bitmap.data.length], 10) < parseInt(img.bitmap.data[j], 10)) {
+            img.bitmap.data[j] = img.bitmap.data[(j * data.theRandom) % img.bitmap.data.length];
         }
     }
 
