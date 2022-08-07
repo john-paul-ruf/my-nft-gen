7f47c1b12ef589d51ed3548585e6877231fb23a9
john.paul.ruf <john.paul.ruf@gmail.com>
2022-08-07 03:12:50 -0500

    Refactor: write directly to mp4 without writing to gif first

--- src/output/writeToMp4.js
+++ src/output/writeToMp4.js
@@ -1,35 +1,34 @@
 import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
 import ffprobe from "@ffprobe-installer/ffprobe";
 import ffmpeg from "fluent-ffmpeg";
-import {IMAGESIZE} from "../logic/gobals.js";
-import {writeFramesToGif} from "./writeFramesToGif.js";
-import fs from "fs";
 
-export const writeToMp4 = async (frameFilenames, config) => {
+export const writeToMp4 = async (fileSelector, config) => {
 
-
-    await writeFramesToGif(frameFilenames, config)
-
-    const maker = ffmpeg().setFfprobePath(ffprobe.path).setFfmpegPath(ffmpegInstaller.path)
 
     const writeMp4 = async () => {
-        return new Promise((resolve) => {
-            maker
-                .size(IMAGESIZE.toString() + 'x' + IMAGESIZE.toString())
-                .input(config.fileOut + '.gif')
+        return new Promise((resolve, reject) => {
+
+            const video = ffmpeg().setFfprobePath(ffprobe.path).setFfmpegPath(ffmpegInstaller.path);
+
+            video.addInput(fileSelector)
+                .outputFormat('mp4')
+                .videoCodec('libx264')
                 .outputOptions('-pix_fmt yuv420p')
-                .output(config.fileOut + '.mp4')
-                .noAudio()
                 .on("end", () => {
                     resolve();
                     console.log("mp4 Finished");
                 })
-                .on("error", (e) => console.log(e))
-                .run();
+                .on('progress', (progress) => {
+                    console.log(progress);
+                })
+                .on("error", (e) => {
+                    reject();
+                    console.log(e)
+                })
+                .mergeToFile(config.fileOut + '.mp4');
+
         });
     }
 
     await writeMp4();
-
-    fs.unlinkSync(config.fileOut + '.gif');
 }
