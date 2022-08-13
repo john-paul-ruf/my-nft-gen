22ae0d0b50bf17ec592632e67a6c21f7351c8d05
john.paul.ruf <john.paul.ruf@gmail.com>
2022-08-13 18:50:54 -0500

    encapsulate globals after running tests

--- src/logic/core/gobals.js
+++ src/logic/core/gobals.js
@@ -1,41 +1,47 @@
 //Encapsulated globals are less bad...
-
 import {getRandomIntExclusive, randomNumber} from "../math/random.js";
-import {getColorBucket} from "../utils/getColorBucket.js";
+import ColorScheme from "color-scheme";
 
 //'mono', 'contrast', 'triade', 'tetrade', 'analogic'.
 const schemeBucket = ['contrast'];
-
 //'default', 'pastel', 'soft', 'light', 'hard', 'pale'
 const variationBucket = ['hard'];
 
-const globals = {
-    layerStrategy: 'sharp',
-    canvasStrategy: 'node-canvas',
+class globalSettings {
+    constructor() {
+        this.layerStrategy = 'sharp';
+        this.canvasStrategy = 'node-canvas';
+        this.workingDirectory = `src/img/working/`;
+        this.finalImageHeight = 1080;
+        this.finalImageWidth = 1920;
+        this.neutrals = [
+            '#2d2d2d',
+            '#1F1F1F',
+            '#080808',
+            /*    '#5b5b5b',*/
+            /*  '#f7f5fa',
+              '#eeeeee',
+              '#F0F3F3',
+              '#cbdae1'*/
+        ];
+        this.scheme = schemeBucket[getRandomIntExclusive(0, schemeBucket.length)];
+        this.variations = variationBucket[getRandomIntExclusive(0, variationBucket.length)];
+        this.hue = getRandomIntExclusive(0, 360);
+        this.distance = randomNumber(0.6, 1);
+        this.colorBucket = this.getColorBucket()
+    }
 
-    workingDirectory: `src/img/working/`,
-
-    finalImageHeight: 1080,
-    finalImageWidth: 1920,
-
-    neutrals: [
-        '#2d2d2d',
-        '#1F1F1F',
-        '#080808',
-        /*    '#5b5b5b',*/
-        /*  '#f7f5fa',
-          '#eeeeee',
-          '#F0F3F3',
-          '#cbdae1'*/
-    ],
-
-    scheme: schemeBucket[getRandomIntExclusive(0, schemeBucket.length)],
-    variations: variationBucket[getRandomIntExclusive(0, variationBucket.length)],
-    hue: getRandomIntExclusive(0, 360),
-    distance: randomNumber(0.6, 1),
-
-    colorBucket: getColorBucket(), //sets palette for entire run... Not sure how I feel about this...
+    getColorBucket() {
+        const bucket = new ColorScheme();
+        return bucket.from_hue(this.hue)
+            .scheme(this.scheme)
+            .distance(this.distance)
+            .variation(this.variations)
+            .colors();
+    }
 }
+
+const globals = new globalSettings();
 
 export const getColorFromBucket = () => {
     return '#' + globals.colorBucket[getRandomIntExclusive(0, globals.colorBucket.length)];
