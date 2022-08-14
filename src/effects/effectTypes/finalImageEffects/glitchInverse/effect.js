cf05363cad026b6537ba5e0050f0f5141f2e5f84
john.paul.ruf <john.paul.ruf@gmail.com>
2022-08-14 08:29:40 -0500

    continuing to extract into items into own files.

--- src/effects/effectTypes/finalImageEffects/glitchInverse/effect.js
+++ src/effects/effectTypes/finalImageEffects/glitchInverse/effect.js
@@ -2,7 +2,7 @@ import {glitchInverse} from "./invoke.js";
 import {generate} from "./generate.js";
 
 export const effect = {
-    invoke: (layer, data) => glitchInverse(layer, data)
+    invoke: (layer) => glitchInverse(layer)
 }
 
 export const glitchInverseEffect = {
