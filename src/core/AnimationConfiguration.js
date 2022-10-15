749e9cd6c05381ad759cdd3456c24f2307bf9089
john.paul.ruf <john.paul.ruf@gmail.com>
2022-10-14 21:04:25 -0500

    the fade out run

--- src/core/AnimationConfiguration.js
+++ src/core/AnimationConfiguration.js
@@ -11,7 +11,7 @@ export class animationConfiguration {
 
         //For compose info
         this._INVOKER_ = 'John Ruf';
-        this.runName = 'LIFTED 3D'
+        this.runName = 'the fade out'
 
         //For testing, render every x frame.
         this.frameInc = 1;
