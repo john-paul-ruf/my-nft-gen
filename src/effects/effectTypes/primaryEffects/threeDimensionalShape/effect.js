db6fb869db494e3eb0398bf83d0dc659a9981e22
john.paul.ruf <john.paul.ruf@gmail.com>
2022-10-18 20:41:58 -0500

    uno mas, ugh

--- src/effects/effectTypes/primaryEffects/threeDimensionalShape/effect.js
+++ src/effects/effectTypes/primaryEffects/threeDimensionalShape/effect.js
@@ -9,7 +9,7 @@ export const threeDimensionalShapeEffect = {
     name: 'three-dimensional-shape',
     generateData: generate,
     effect: effect,
-    effectChance: 20s,
+    effectChance: 20,
     requiresLayer: true,
 }
 
