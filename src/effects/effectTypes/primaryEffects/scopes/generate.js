4913916f8043575602dc980f3504a3e57b47db68
john.paul.ruf <john.paul.ruf@gmail.com>
2022-10-19 20:47:27 -0500

    the return of the hex run

--- src/effects/effectTypes/primaryEffects/scopes/generate.js
+++ src/effects/effectTypes/primaryEffects/scopes/generate.js
@@ -5,8 +5,8 @@ import {scopesEffect} from "./effect.js";
 const finalImageSize = getFinalImageSize();
 
 const config = {
-    sparsityFactor: {lower: .5, upper: 1},
-    gapFactor: {lower: 0.1, upper: 0.3},
+    sparsityFactor: {lower: 2, upper: 3},
+    gapFactor: {lower: 0.3, upper: 0.5},
     radiusFactor: {lower: 0.1, upper: 0.2},
     scaleFactor: 2,
     alphaRange: {bottom: {lower: 0.15, upper: 0.2}, top: {lower: 0.25, upper: 0.4}},
