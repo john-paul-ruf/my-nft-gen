f86041496a9ef037ece9d0cd5dae76cdc3c86354
john.paul.ruf <john.paul.ruf@gmail.com>
2022-10-22 16:46:31 -0500

    lens flare effect complete, for now...

--- src/effects/effectTypes/primaryEffects/lensFlare/effect.js
+++ src/effects/effectTypes/primaryEffects/lensFlare/effect.js
@@ -9,7 +9,7 @@ export const lensFlareEffect = {
     name: 'lens-flare',
     generateData: generate,
     effect: effect,
-    effectChance: 100,
+    effectChance: 35,
     requiresLayer: false,
 }
 
