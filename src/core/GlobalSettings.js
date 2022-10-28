b88f5baa2024ebe8ed95da9dfc46898b6d25f2f5
john.paul.ruf <john.paul.ruf@gmail.com>
2022-10-28 13:17:51 -0500

    flipping the bits

--- src/core/GlobalSettings.js
+++ src/core/GlobalSettings.js
@@ -17,7 +17,7 @@ class globalSettings {
 
         //For 2D palettes
         this.neutrals = [
-            '#FFFFFF',/*
+            '#333333',/*
             '#1F1F1F',
             '#5b5b5b',
             '#7f7f7f',*/];
