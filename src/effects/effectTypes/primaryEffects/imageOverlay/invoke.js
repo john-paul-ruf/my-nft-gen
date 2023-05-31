11265025e091973a56bdd7fba6dab80125a5d397
john.paul.ruf <john.paul.ruf@gmail.com>
2023-05-31 06:34:36 -0500

    the mind thief - bit flips

--- src/effects/effectTypes/primaryEffects/imageOverlay/invoke.js
+++ src/effects/effectTypes/primaryEffects/imageOverlay/invoke.js
@@ -4,6 +4,6 @@ import {getFinalImageSize} from "../../../../core/GlobalSettings.js";
 export const imageOverlay = async (layer, data) => {
     let tempLayer = await LayerFactory.getLayerFromFile(data.imageOverlay);
     const finalSize = getFinalImageSize();
-    await tempLayer.resize(finalSize.height, finalSize.width);
+    await tempLayer.resize(finalSize.height - 250, finalSize.width - 250);
     await layer.compositeLayerOver(tempLayer)
 }
--- src/effects/effectTypes/primaryEffects/mappedFrames/effect.js
+++ src/effects/effectTypes/primaryEffects/mappedFrames/effect.js
@@ -9,7 +9,7 @@ export const mappedFramesEffect = {
     name: 'mapped-frames',
     generateData: generate,
     effect: effect,
-    effectChance: 35,
+    effectChance: 0,
     requiresLayer: true,
     baseLayer: true,
 }
