ba6e719e99f1494cce5e8140bbad9c5b66a4f745
john.paul.ruf <john.paul.ruf@gmail.com>
2023-06-25 07:29:28 -0500

    standard lo-fi prints run

--- src/effects/effectTypes/secondaryEffects/single-layer-glitch-drumroll-horizontal-wave/effect.js
+++ src/effects/effectTypes/secondaryEffects/single-layer-glitch-drumroll-horizontal-wave/effect.js
@@ -9,7 +9,7 @@ export const singleLayerGlitchDrumrollHorizontalWaveEffect = {
     name: 'single-layer-glitch-drumroll-horizontal-wave',
     generateData: generate,
     effect: effect,
-    effectChance: 40,
+    effectChance: 0,
     requiresLayer: false,
 }
 
