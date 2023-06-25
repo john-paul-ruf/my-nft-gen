f912f9dbfb216d97507ae48b2e612e786fcf1bcf
john.paul.ruf <john.paul.ruf@gmail.com>
2023-06-25 07:32:09 -0500

    standard lo-fi prints run

--- src/effects/effectTypes/primaryEffects/mappedFrames/effect.js
+++ src/effects/effectTypes/primaryEffects/mappedFrames/effect.js
@@ -9,7 +9,7 @@ export const mappedFramesEffect = {
     name: 'mapped-frames',
     generateData: generate,
     effect: effect,
-    effectChance: 0,
+    effectChance: 20,
     requiresLayer: true,
     baseLayer: true,
 }
