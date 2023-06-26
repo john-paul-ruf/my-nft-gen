601b0ee36b6ddc45d4b8519565244ef66012a6cd
john.paul.ruf <john.paul.ruf@gmail.com>
2023-06-26 13:58:42 -0500

    standard lo-fi prints run

--- src/effects/effectTypes/primaryEffects/scanLines/generate.js
+++ src/effects/effectTypes/primaryEffects/scanLines/generate.js
@@ -3,13 +3,13 @@ import {getColorFromBucket, getFinalImageSize,} from "../../../../core/GlobalSet
 import {verticalScanLinesEffect} from "./effect.js";
 
 const config = {
-    lines: {lower: 4, upper: 8},
+    lines: {lower: 3, upper: 6},
     minlength: {lower: 5, upper: 25},
     maxlength: {lower: 740, upper: 75},
     times: {lower: 4, upper: 8},
     alphaRange: {bottom: {lower: 0.3, upper: 0.4}, top: {lower: 0.5, upper: 0.6}},
     alphaTimes: {lower: 4, upper: 8},
-    loopTimes: {lower: 1, upper: 3},
+    loopTimes: {lower: 1, upper: 2},
 }
 
 const getPixelTrailLength = () => {
